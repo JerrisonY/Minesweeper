e19e6d5fba8026f167fc1078225a99f34b621462
Jerrison Yip <jerryyip24@gmail.com>
2023-06-30 19:49:45 -0400

    added setMines function

--- js/script.js
+++ js/script.js
@@ -4,14 +4,16 @@ const mineCount = document.querySelector('#minecount')
 const board = document.querySelector('#board');
 
 
+let gameBoard = [];
 let rows = 16;
 let columns = 16;
-let mines = 0;
+
+let minesCount = 5;
+let minesLocation = [];
 
 renderBoard()
 
 function renderBoard() {
-
     board.style.setProperty('--rows', rows);
     board.style.setProperty('--columns', columns);
 
@@ -24,17 +26,32 @@ function renderBoard() {
             board.appendChild(tile)
             row.push(tile)
         }
-        // board.push(row)
+        gameBoard.push(row);
     }
 }
 
+function setMines() {
+    let mineTracker = minesCount;
+    
+    while (mineTracker > 0) {
+        let row = Math.floor(Math.random() * rows);
+        let column = Math.floor(Math.random() * columns);
+        let id = `${row}-${column}`;
+        
+        if (!minesLocation.includes(id)) {
+            minesLocation.push(id)
+            mineTracker -= 1;
+        }
+    }
+}
 
+setMines()
 
 darkMode.addEventListener('click', () => {
     mainContainer.classList.toggle('dark-mode')
     mainContainer.classList.toggle('light-mode')
     if (darkMode.innerHTML === '<i class="fa-solid fa-moon"></i>Dark') {
-        darkMode.innerHTML = '<i class="fa-solid fa-sun" style="color: #ffffff;"></i>Light';
+        darkMode.innerHTML = '<i class="fa-solid fa-sun" style="color: #ffffff;"></i><span style="color: #fff;"> Light</span>';
     } else {
         darkMode.innerHTML = '<i class="fa-solid fa-moon"></i>Dark'
     }
